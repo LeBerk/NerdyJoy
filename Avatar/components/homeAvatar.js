57f36b158bc30b9d8d41d1f56e5fb99cc9bcc669
SjainThuli <72549874+SjainThuli@users.noreply.github.com>
2021-08-27 15:00:18 +0200

    Add files via upload

---
+++ Avatar/components/homeAvatar.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Image, Text, View, TouchableOpacity } from 'react-native';
+
+export default function homeAvatar({navigation}) {
+    return (
+      <View style={styles.container}>
+          <Text style={{fontWeight: 'bold', fontSize: 45}}>Meet Joy,your new assitant! He is here to motivate 
+          you and make learning fun!
+        </Text>
+          <Image source={require('../assets/Group29.png')}/>
+        <View style={styles.buttoncontainer}>
+          <TouchableOpacity 
+            onPress={()=>navigation.navigate('editAvatar')}>
+            <Text style={styles.button}>Edit Avatar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttoncontainers}> 
+        <TouchableOpacity 
+            onPress={()=>navigation.navigate('editAvatar')}>
+            <Text style={styles.button}>Continue</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  intro: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 18,
+  },
+  buttoncontainer: {
+    margin: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 7,
+    borderRadius: 5,
+    backgroundColor: "black",
+    marginRight: 100
+  }, 
+  buttoncontainers: {
+    margin: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 7,
+    borderRadius: 5,
+    backgroundColor: "black",
+    marginRight: 100,
+    left: 130,
+    bottom: 50
+  },
+  button: {
+   fontSize: 15,
+   color: 'white',
+   fontWeight: 'bold'
+  },
+  continue: {
+   fontSize: 15,
+   color: 'white',
+   fontWeight: 'bold'
+  },
+});
